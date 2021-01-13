849d4840c4aefea7f8482e7d77a635e53d9caace
Vaibhav Sharma <vaisharma16@gmail.com>
2021-01-13 14:05:50 +0530

    done

--- cafe/settings.py
+++ cafe/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL="/media/"
-MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+#MEDIA_URL="/media/"
 MEDIA_URL = "/image/download/"
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+
+#MEDIA_ROOT = BASE_DIR
--- home/views.py
+++ home/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import Customer
 # Create your views here.
 #def reg(request):
-
- #   return render(request,'regform.html')
+           
 
 def preview(request):
 
@@ -27,37 +26,8 @@ class Register(View):
         emp_id = postData.get('emp_id')
         mob = postData.get('mob')
         email = postData.get('email')
-        
-        # validation
-        value = {'fname': fname,'oname': oname,'emp_id': emp_id, 'mob': mob, 'email': email}
-        customer = Customer(fname=fname,oname=oname,emp_id=emp_id, mob=mob, email=email)
-        err_msg = self.validateCustomer(customer)
-
-        # saving
-        if not err_msg:
-            
-            customer.register()
-            return HttpResponse("Register Sucessfull")
-        else:
-            data = {'error': err_msg, 'values': value}
-            return render(request, "regform.html", data)
-
-    def validateCustomer(self,customer):
-        err_msg = None
-        if (not customer.fname):
-            err_msg = "Name Required!"
-        elif (not customer.oname):
-            err_msg = "Organization Name. required"
-        elif (not customer.emp_id):
-            err_msg = "Employee Id. required"
-        elif (not customer.mob):
-            err_msg = "Phone No. required"
-        elif not customer.validatePhone():
-            err_msg = "Enter valid Phone no."
-        elif len(customer.mob) < 10:
-            err_msg = "Phone No. must have 10 digits"
-        elif not customer.validateEmail():
-            err_msg = 'Enter valid email'
-        elif customer.doExists():
-            err_msg = 'Email Address Already registered..'
-        return err_msg
+        image = request.FILES['image']
+        customer = Customer(fname=fname,oname=oname,emp_id=emp_id, mob=mob, email=email,image=image)
+        customer.register()
+        return HttpResponse("Sucess")
+       
